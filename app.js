4c6704cf58f536982c5f010e504b5c9c2a070f55
hsperez6 <hsperez@gmail.com>
2023-08-14 23:37:01 -0700

    Update app.js

--- app.js
+++ app.js
@@ -82,6 +82,40 @@ const Planet = (props) => {
   );
 };
 
+class Planet extends React.Component {
+
+  state = {
+    score: 0
+  }
+
+  render() {
+    return (
+      <div class="container"> 
+        <div class="card">
+          <div>
+            <img src="img/mercury.jpg" alt="Mercury">
+          </div>
+          <h2>Mercury</h2>
+          <p>Mercury is the closest planet to the Sun. Due to its proximity, it's not easily seen except during twilight...</p>
+          <h3>Planet Profile</h3>
+          <ul>
+            <li><strong>Diameter:</strong> 3,031.67 mi</li>
+            <li><strong>Moons:</strong> none</li>
+          </ul>
+        </div>
+        <div class="card">...</div>
+      </div>  
+    );
+  }
+};
+
+
+
+
+
+
+
+
 
 const Container = (props) => {
   return (
